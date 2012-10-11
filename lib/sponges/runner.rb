6746ab96f6ed544883e80dacd1572f9524f66679
chatgris <jboyer@af83.com>
2012-10-11 18:48:31 +0200

    Fix Process.daemon
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/sponges/runner.rb
+++ lib/sponges/runner.rb
@@ -12,16 +12,15 @@ module Sponges
     end
 
     def work(worker, method, *args, &block)
+      if daemonize?
+        Sponges.logger.info "Supervisor daemonized."
+        Process.daemon
+      end
       Sponges.logger.info "Runner #{@name} start message received."
       @supervisor = fork_supervisor(worker, method, *args, &block)
       trap_signals
       Sponges.logger.info "Supervisor started with #{@supervisor} pid."
-      if daemonize?
-        Sponges.logger.info "Supervisor daemonized."
-        Process.daemon
-      else
-        Process.waitpid(@supervisor) unless daemonize?
-      end
+      Process.waitpid(@supervisor) unless daemonize?
     end
 
     private

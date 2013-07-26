9d625e162c53d58e99aa6732381bbd083a015def
chatgris <jboyer@af83.com>
2013-07-26 20:39:04 +0200

    Less noise with `module_function`
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/sponges.rb
+++ lib/sponges.rb
@@ -23,26 +23,24 @@ module Sponges
   STOP_SIGNALS = [:INT, :QUIT, :TERM]
   SIGNALS = STOP_SIGNALS + [:HUP, :TTIN, :TTOU, :CHLD]
 
+  module_function
+
   def configure(&block)
     Sponges::Configuration.configure &block
   end
-  module_function :configure
 
   def start(worker_name, options = {}, argv = ARGV, &block)
     Sponges::Configuration.worker_name = worker_name
     Sponges::Configuration.worker = block
     Sponges::Cli.start(argv)
   end
-  module_function :start
 
   def logger
     return @logger if @logger
     @logger = Sponges::Configuration.logger || Logger.new(STDOUT)
   end
-  module_function :logger
 
   def env
     Sponges::Configuration.env
   end
-  module_function :env
 end
